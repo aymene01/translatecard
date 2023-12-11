1e055ad2b7e407210451dc3d2691aa1e3aef9446
aymene01 <aymene01@hotmail.fr>
2023-12-11 16:24:59 +0100

    rollback

--- apps/api/src/business/domains/card/updateCardById.ts
+++ apps/api/src/business/domains/card/updateCardById.ts
@@ -1,69 +1,60 @@
-import { Options } from '@/business/types'
-import { Card, Prisma } from '@prisma/client'
+import { Options } from "@/business/types";
+import { Card } from "@prisma/client";
 
 type DataToUpdate = {
-  content?: string
-  traduction?: string
-  title?: string
-}
+  content?: string;
+  traduction?: string;
+  title?: string;
+};
 
 type UpdateCardByIdRequest = {
-  id: number
-} & Partial<DataToUpdate>
+  id: number;
+} & Partial<DataToUpdate>;
 
-type UpdateCardByIdResponse = { updatedCard: Card } | { error: { message: string } }
+type UpdateCardByIdResponse = 
+  | { updatedCard: Card } 
+  | { error: { message: string } };
 
-export const updateCardById = async (opts: Options, req: UpdateCardByIdRequest): Promise<UpdateCardByIdResponse> => {
-  const updatePayload = buildUpdatePayload(req)
+export const updateCardById = async (
+  opts: Options, 
+  req: UpdateCardByIdRequest
+): Promise<UpdateCardByIdResponse> => {
 
-  if (Object.keys(updatePayload).length === 0) {
+  const dataToUpdate: Partial<DataToUpdate> = {};
+
+  if (typeof req.content !== 'undefined') dataToUpdate.content = req.content;
+  if (typeof req.traduction !== 'undefined') dataToUpdate.traduction = req.traduction;
+  if (typeof req.title !== 'undefined') dataToUpdate.title = req.title;
+
+  const existingCard = await opts.database.prisma.card.findUnique({
+    where: {
+      id: req.id,
+    },
+  });
+
+  if (!existingCard) {
     return {
       error: {
-        message: 'No valid fields provided for update.',
+        message: `Card with ID ${req.id} does not exist.`,
       },
-    }
+    };
   }
 
   try {
     const updatedCard = await opts.database.prisma.card.update({
       where: { id: req.id },
-      data: updatePayload,
-    })
+      data: dataToUpdate,
+    });
 
-    return { updatedCard }
-  } catch (error) {
-    opts.logger.error(`Error updating card with ID ${req.id}:`, error)
+    return { updatedCard };
 
-    if (isRecordNotFoundError(error)) {
-      return {
-        error: {
-          message: `Card with ID ${req.id} does not exist.`,
-        },
-      }
-    }
+  } catch (error: unknown) {
+    opts.logger.error(`Error updating card with ID ${req.id}:`, error);
 
     return {
       error: {
         message: 'Error updating card data. Please try again later.',
       },
-    }
+    };
   }
-}
-
-function buildUpdatePayload(req: UpdateCardByIdRequest): Prisma.CardUpdateInput {
-  const { id, ...fields } = req
-  return Object.entries(fields).reduce((payload, [key, value]) => {
-    if (value !== undefined) {
-      payload[key as keyof DataToUpdate] = value
-    }
-    return payload
-  }, {} as Prisma.CardUpdateInput)
-}
-
-function isRecordNotFoundError(error: unknown): boolean {
-  return isPrismaError(error) && error.meta?.cause === 'Record to delete does not exist.'
-}
-
-function isPrismaError(error: unknown): error is Prisma.PrismaClientKnownRequestError {
-  return typeof error === 'object' && error !== null && 'code' in error
-}
+};
