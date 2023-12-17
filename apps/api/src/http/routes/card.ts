ade4e7dd8a71b35b031bff36a0d119e2fb1ca63e
aymene01 <aymene01@hotmail.fr>
2023-12-17 12:46:38 +0100

    prettier

--- apps/api/src/http/routes/card.ts
+++ apps/api/src/http/routes/card.ts
@@ -1,55 +1,55 @@
-import { Router, Request, Response } from 'express';
-import { sendErrorResponse, sendSuccessResponse, parseIdOrSendError } from '@/http/utils';
+import { Router, Request, Response } from 'express'
+import { sendErrorResponse, sendSuccessResponse, parseIdOrSendError } from '@/http/utils'
 
-const api: Router = Router();
+const api: Router = Router()
 
 api.get('/', async (req: Request, res: Response) => {
-  const response = await req.business.getAllCards();
-  if('error' in response) {
-    return sendErrorResponse(res, response.error);
+  const response = await req.business.getAllCards()
+  if ('error' in response) {
+    return sendErrorResponse(res, response.error)
   }
-  return sendSuccessResponse(res, response);
-});
+  return sendSuccessResponse(res, response)
+})
 
 api.get('/:id', async (req: Request, res: Response) => {
-  const id = parseIdOrSendError(req, res);
+  const id = parseIdOrSendError(req, res)
   if (id) {
-    const response = await req.business.getCardById({ id });
+    const response = await req.business.getCardById({ id })
     if ('error' in response) {
-      return sendErrorResponse(res, response.error);
+      return sendErrorResponse(res, response.error)
     }
-    return sendSuccessResponse(res, response);
+    return sendSuccessResponse(res, response)
   }
-});
+})
 
 api.post('/', async (req: Request, res: Response) => {
-  const response = await req.business.createCard(req.body);
-  if('error' in response) {
-    return sendErrorResponse(res, response.error);
+  const response = await req.business.createCard(req.body)
+  if ('error' in response) {
+    return sendErrorResponse(res, response.error)
   }
-  return sendSuccessResponse(res, response);
-});
+  return sendSuccessResponse(res, response)
+})
 
 api.delete('/:id', async (req: Request, res: Response) => {
-  const id = parseIdOrSendError(req, res);
+  const id = parseIdOrSendError(req, res)
   if (id) {
-    const response = await req.business.deleteCardById({ id });
+    const response = await req.business.deleteCardById({ id })
     if ('error' in response) {
-      return sendErrorResponse(res, response.error);
+      return sendErrorResponse(res, response.error)
     }
-    return sendSuccessResponse(res, response);
+    return sendSuccessResponse(res, response)
   }
-});
+})
 
 api.put('/:id', async (req: Request, res: Response) => {
-  const id = parseIdOrSendError(req, res);
+  const id = parseIdOrSendError(req, res)
   if (id) {
-    const response = await req.business.updateCardById({ id, ...req.body });
+    const response = await req.business.updateCardById({ id, ...req.body })
     if ('error' in response) {
-      return sendErrorResponse(res, response.error);
+      return sendErrorResponse(res, response.error)
     }
-    return sendSuccessResponse(res, response);
+    return sendSuccessResponse(res, response)
   }
-});
+})
 
-export default api;
+export default api
--- apps/api/src/http/utils.ts
+++ apps/api/src/http/utils.ts
@@ -1,18 +1,18 @@
-import { Request, Response } from 'express';
+import { Request, Response } from 'express'
 
-export const sendErrorResponse = (res: Response, error: { status: number, message: string }) => {
-    res.status(error.status).json({ error: error.message });
-  };
-  
-export const sendSuccessResponse = (res: Response, { data, status }: { data: any, status: number }) => {
-    res.status(status).json(data);
-  };
-  
+export const sendErrorResponse = (res: Response, error: { status: number; message: string }) => {
+  res.status(error.status).json({ error: error.message })
+}
+
+export const sendSuccessResponse = (res: Response, { data, status }: { data: any; status: number }) => {
+  res.status(status).json(data)
+}
+
 export const parseIdOrSendError = (req: Request, res: Response): number | null => {
-    const id = req.business.parseId(req.params.id);
-    if (!id) {
-      sendErrorResponse(res, { status: 400, message: 'Invalid ID format' });
-      return null;
-    }
-    return id;
-  };
+  const id = req.business.parseId(req.params.id)
+  if (!id) {
+    sendErrorResponse(res, { status: 400, message: 'Invalid ID format' })
+    return null
+  }
+  return id
+}
